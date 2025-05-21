20fcbe603d0e703745995ecd36ed97a3f029e7be
SeungJongOh <tmdwhd1213@dfcc.co.kr>
2025-05-21 15:15:34 +0900

    env 파일 추가

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -23,8 +23,8 @@ import { EmailVerification } from './entities/email-verification.entity';
       transport: {
         service: 'Gmail',
         auth: {
-          user: 'tmdwhd1214@gmail.com',
-          pass: 'ygno zoln xvjh hcwt',
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
         },
       },
       defaults: {
